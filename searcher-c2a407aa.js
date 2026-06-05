95a5affae4ea142d5749a8ed890f9dcd63ac8290
dicethedev <dicethedev@users.noreply.github.com>
2026-06-05 10:13:37 +0000

    deploy: 85115d79654ce5204effbbb45bd0f9bca363333a

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-eac69af8.js',
+                path_to_root + 'searchindex-f7745e2b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
